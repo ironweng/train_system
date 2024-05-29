0c106b0039b8b957e7b2970bc71d99487c25c459
zhaopei <424481223@qq.com>
2024-05-29 11:26:10 +0800

    4-4 增加启动日志，增加/member 模块访问前缀

--- member/src/main/java/com/zhaopei/train/member/config/MemberApplication.java
+++ member/src/main/java/com/zhaopei/train/member/config/MemberApplication.java
@@ -1,15 +1,21 @@
 package com.zhaopei.train.member.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
+@Slf4j
 @SpringBootApplication
 @ComponentScan("com.zhaopei")
 public class MemberApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app=new SpringApplication(MemberApplication.class);
+        Environment env=app.run(args).getEnvironment();
+        log.info("启动成功!");
+        log.info("地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
