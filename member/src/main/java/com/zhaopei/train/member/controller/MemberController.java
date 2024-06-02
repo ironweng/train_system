95c08708a5326b3c21c6801e64c5c913819ee65d
zhaopei <424481223@qq.com>
2024-06-02 21:16:28 +0800

    5-9 前后端联调完成短信验证码登录功能

--- member/src/main/java/com/zhaopei/train/member/controller/MemberController.java
+++ member/src/main/java/com/zhaopei/train/member/controller/MemberController.java
@@ -45,7 +45,7 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public CommonResp<MemberLoginResp> login(@Valid MemberLoginReq req){
+    public CommonResp<MemberLoginResp> login(@Valid @RequestBody MemberLoginReq req){
         MemberLoginResp resp = memberService.login(req);
         return new CommonResp<>(resp);
     }
--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -6,8 +6,6 @@
           :model="loginForm"
           name="basic"
           autocomplete="off"
-          @finish="onFinish"
-          @finishFailed="onFinishFailed"
       >
         <a-form-item
             label=""
@@ -42,6 +40,8 @@
 <script>
 import { defineComponent, reactive } from 'vue';
 import axios from "axios";
+import { notification } from 'ant-design-vue';
+
 export default defineComponent({
   name: "login-view",
   setup() {
@@ -50,27 +50,37 @@ export default defineComponent({
       code: '',
     });
 
-    const onFinish = values => {
-      console.log('Success:', values);
-    };
-
-    const onFinishFailed = errorInfo => {
-      console.log('Failed:', errorInfo);
-    };
-
     const sendCode = () => {
-      axios.post("http://localhost:8000/member/member/send-code",{
-        mobile:loginForm.mobile
+      axios.post("http://localhost:8000/member/member/send-code", {
+        mobile: loginForm.mobile
       }).then(response => {
         console.log(response);
+        let data = response.data;
+        if (data.success) {
+          notification.success({ description: '发送验证码成功！' });
+          loginForm.code = "8888";
+        } else {
+          notification.error({ description: data.message });
+        }
       });
+    };
+
+    const login = () => {
+      axios.post("http://localhost:8000/member/member/login", loginForm).then((response) => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({ description: '登录成功！' });
+          console.log("登录成功:",data.content);
+        } else {
+          notification.error({ description: data.message });
+        }
+      })
     };
 
     return {
       loginForm,
-      onFinish,
-      onFinishFailed,
-      sendCode
+      sendCode,
+      login
     };
   },
 });
