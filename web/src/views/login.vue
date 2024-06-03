12b42a2125e9a7256bba59717b046111e8d7903c
zhaopei <424481223@qq.com>
2024-06-03 23:15:45 +0800

    5-10 增加axios拦截器，在前端控制台也能打印请求参数和返回结果

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -54,7 +54,6 @@ export default defineComponent({
       axios.post("http://localhost:8000/member/member/send-code", {
         mobile: loginForm.mobile
       }).then(response => {
-        console.log(response);
         let data = response.data;
         if (data.success) {
           notification.success({ description: '发送验证码成功！' });
@@ -70,7 +69,6 @@ export default defineComponent({
         let data = response.data;
         if (data.success) {
           notification.success({ description: '登录成功！' });
-          console.log("登录成功:",data.content);
         } else {
           notification.error({ description: data.message });
         }
