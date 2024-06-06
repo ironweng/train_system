b40ca83a111c712b2fca565ac66979f86af558fb
zhaopei <424481223@qq.com>
2024-06-06 15:13:41 +0800

    7-9 封装分页返回结果(原理同封装分页请求参数)

--- member/src/main/java/com/zhaopei/train/member/controller/PassengerController.java
+++ member/src/main/java/com/zhaopei/train/member/controller/PassengerController.java
@@ -3,6 +3,7 @@ package com.zhaopei.train.member.controller;
 
 import com.zhaopei.train.common.context.LoginMemberContext;
 import com.zhaopei.train.common.resp.CommonResp;
+import com.zhaopei.train.common.resp.PageResp;
 import com.zhaopei.train.member.req.PassengerQueryReq;
 import com.zhaopei.train.member.req.PassengerSaveReq;
 import com.zhaopei.train.member.resp.PassengerQueryResp;
@@ -10,8 +11,6 @@ import com.zhaopei.train.member.service.PassengerService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
@@ -27,9 +26,9 @@ public class PassengerController {
     }
 
     @GetMapping("/query-list")
-    public CommonResp<List<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req){
+    public CommonResp<PageResp<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req){
         req.setMemberId(LoginMemberContext.getId());
-        List<PassengerQueryResp> list = passengerService.queryList(req);
+        PageResp<PassengerQueryResp> list = passengerService.queryList(req);
         return new CommonResp<>(list);
     }
 }
--- member/src/main/java/com/zhaopei/train/member/service/PassengerService.java
+++ member/src/main/java/com/zhaopei/train/member/service/PassengerService.java
@@ -4,7 +4,9 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjUtil;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.zhaopei.train.common.context.LoginMemberContext;
+import com.zhaopei.train.common.resp.PageResp;
 import com.zhaopei.train.common.util.SnowUtil;
 import com.zhaopei.train.member.domain.Passenger;
 import com.zhaopei.train.member.domain.PassengerExample;
@@ -12,11 +14,13 @@ import com.zhaopei.train.member.mapper.PassengerMapper;
 import com.zhaopei.train.member.req.PassengerQueryReq;
 import com.zhaopei.train.member.req.PassengerSaveReq;
 import com.zhaopei.train.member.resp.PassengerQueryResp;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class PassengerService {
 
@@ -34,14 +38,27 @@ public class PassengerService {
         passengerMapper.insert(passenger);
     }
 
-    public List<PassengerQueryResp> queryList(PassengerQueryReq req){
+    public PageResp<PassengerQueryResp> queryList(PassengerQueryReq req){
         PassengerExample passengerExample=new PassengerExample();
         PassengerExample.Criteria criteria = passengerExample.createCriteria();
         if(ObjUtil.isNotNull(req.getMemberId())){
             criteria.andMemberIdEqualTo(req.getMemberId());
         }
+
+        log.info("req查询页码:{}",req.getPage());
+        log.info("req每页条数:{}",req.getSize());
         PageHelper.startPage(req.getPage(), req.getSize());
         List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
-        return BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
+
+        //PageInfo的底层就是select ... count()... 即返回总条数
+        PageInfo<Passenger> pageInfo=new PageInfo<>(passengerList);
+        log.info("resp总条数:{}",pageInfo.getTotal());
+        log.info("总页数:{}",pageInfo.getPages());
+
+        List<PassengerQueryResp> list = BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
+        PageResp<PassengerQueryResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
     }
 }
