838e219e87d8b13e7b0709be20675c8c69d2b492
zhaopei <424481223@qq.com>
2024-06-12 15:04:12 +0800

    7-16 乘车人删除接口开发

--- member/src/main/java/com/zhaopei/train/member/controller/PassengerController.java
+++ member/src/main/java/com/zhaopei/train/member/controller/PassengerController.java
@@ -31,4 +31,10 @@ public class PassengerController {
         PageResp<PassengerQueryResp> list = passengerService.queryList(req);
         return new CommonResp<>(list);
     }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id){
+        passengerService.delete(id);
+        return new CommonResp<>();
+    }
 }
