293b8a9b896a4379fb356c535358ab13ee259806
zhaopei <424481223@qq.com>
2024-06-19 23:36:32 +0800

    8-6 增加DBUtil,获取表字段信息

--- generator/src/main/java/com/zhaopei/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/com/zhaopei/train/generator/gen/ServerGenerator.java
@@ -1,5 +1,7 @@
 package com.zhaopei.train.generator.gen;
 
+import com.zhaopei.train.generator.util.DbUtil;
+import com.zhaopei.train.generator.util.Field;
 import com.zhaopei.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 import org.dom4j.Document;
@@ -10,6 +12,7 @@ import org.dom4j.io.SAXReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ServerGenerator {
@@ -38,6 +41,17 @@ public class ServerGenerator {
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
         System.out.println(tableName.getText() + "/" + domainObjectName.getText());
 
+        // 为DbUtil设置数据源
+        Node connectionURL = document.selectSingleNode("//@connectionURL");
+        Node userId = document.selectSingleNode("//@userId");
+        Node password = document.selectSingleNode("//@password");
+        System.out.println("url: " + connectionURL.getText());
+        System.out.println("user: " + userId.getText());
+        System.out.println("password: " + password.getText());
+        DbUtil.url = connectionURL.getText();
+        DbUtil.user = userId.getText();
+        DbUtil.password = password.getText();
+
         // 示例：表名 zhaopei_test
         // Domain = ZhaopeiTest
         String Domain = domainObjectName.getText();
@@ -45,6 +59,10 @@ public class ServerGenerator {
         String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
         // do_main = zhaopei-test
         String do_main = tableName.getText().replaceAll("_", "-");
+        // 表中文名
+        String tableNameCn = DbUtil.getTableComment(tableName.getText());
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName.getText());
+
 
         //组装参数
         Map<String, Object> param = new HashMap<>();
