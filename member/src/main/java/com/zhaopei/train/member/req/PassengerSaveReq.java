19485fac9c4cde83bc42596b7ae9aedd77ea2586
zhaopei <424481223@qq.com>
2024-07-06 19:21:30 +0800

    8-8 生成saveReq实体类

--- member/src/main/java/com/zhaopei/train/member/req/PassengerSaveReq.java
+++ member/src/main/java/com/zhaopei/train/member/req/PassengerSaveReq.java
@@ -6,23 +6,45 @@ import jakarta.validation.constraints.NotBlank;
 import java.util.Date;
 
 public class PassengerSaveReq {
+
+    /**
+    * id
+    */
     private Long id;
 
+    /**
+    * 会员id
+    */
     private Long memberId;
 
+    /**
+    * 姓名
+    */
     @NotBlank(message = "【姓名】不能为空")
     private String name;
 
-    @NotBlank(message = "【身份证号】不能为空")
+    /**
+    * 身份证
+    */
+    @NotBlank(message = "【身份证】不能为空")
     private String idCard;
 
-    @NotBlank(message = "【乘车人类型】不能为空")
+    /**
+    * 旅客类型|枚举[PassengerTypeEnum]
+    */
+    @NotBlank(message = "【旅客类型】不能为空")
     private String type;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    /**
+    * 新增时间
+    */
+    @JsonFormat(pattern = "HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    /**
+    * 修改时间
+    */
+    @JsonFormat(pattern = "HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 
     public Long getId() {
@@ -83,10 +105,10 @@ public class PassengerSaveReq {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
+    StringBuilder sb = new StringBuilder();
+    sb.append(getClass().getSimpleName());
+    sb.append(" [");
+    sb.append("Hash = ").append(hashCode());
         sb.append(", id=").append(id);
         sb.append(", memberId=").append(memberId);
         sb.append(", name=").append(name);
@@ -94,7 +116,7 @@ public class PassengerSaveReq {
         sb.append(", type=").append(type);
         sb.append(", createTime=").append(createTime);
         sb.append(", updateTime=").append(updateTime);
-        sb.append("]");
-        return sb.toString();
+    sb.append("]");
+    return sb.toString();
     }
 }
