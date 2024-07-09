07c0596bd1c83d5dd16704ae621430bc309f578e
zhaopei <424481223@qq.com>
2024-07-10 00:03:05 +0800

    9-3 创建admin模块，所有的代码都是从web复制。这样是为了保持前端版本的一致，避免出现问题。

---
+++ admin/src/main.js
@@ -0,0 +1,53 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Antd, {notification} from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import * as Icons from '@ant-design/icons-vue';
+import axios from "axios";
+import './assets/js/enums'
+
+const app = createApp(App);
+app.use(Antd).use(store).use(router).mount('#app');
+
+//全局使用图标
+const icons = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数：', config);
+    const _token=store.state.member.token;
+    if(_token){
+        config.headers.token=_token;
+        console.log("请求headers增加token:",_token)
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('返回结果：', response);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    // const response=error.response;
+    const status=401;
+    if(status === 401){
+        //判断状态码是401,跳转到登录页面，因为此时是因为token过期被拦截了
+        console.log("未登录或登录超时，跳转登录页面");
+        store.commit("setMember",{});
+        notification.error({description: "未登录或登录超时"});
+        router.push('/login');
+    }
+    return Promise.reject(error);
+});
+
+axios.defaults.baseURL = process.env.VUE_APP_SERVER;
+console.log('环境：', process.env.NODE_ENV);
+console.log('服务端：', process.env.VUE_APP_SERVER);
---
+++ admin/src/views/main/welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>欢迎使用雨文12306售票系统</h1>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {
+    };
+  },
+});
+</script>
+<style>
+</style>
