a8d5f0883c1c910be04e4e9f0a11cd36a3ebe70d
zhaopei <424481223@qq.com>
2024-07-10 23:53:07 +0800

    9-4 增加business业务模块

--- business/src/main/java/com/zhaopei/train/business/config/BusinessApplication.java
+++ business/src/main/java/com/zhaopei/train/business/config/BusinessApplication.java
@@ -1,7 +1,6 @@
-package com.zhaopei.train.member.config;
+package com.zhaopei.train.business.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,11 +9,11 @@ import org.springframework.core.env.Environment;
 @Slf4j
 @SpringBootApplication
 @ComponentScan("com.zhaopei")
-@MapperScan("com.zhaopei.train.*.mapper")
-public class MemberApplication {
+//@MapperScan("com.zhaopei.train.*.mapper")
+public class BusinessApplication {
 
     public static void main(String[] args) {
-        SpringApplication app=new SpringApplication(MemberApplication.class);
+        SpringApplication app=new SpringApplication(BusinessApplication.class);
         Environment env=app.run(args).getEnvironment();
         log.info("启动成功!");
         log.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
