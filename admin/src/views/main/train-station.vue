7cb722b422211715d33532c74341c0c2f8b9c451
zhaopei <424481223@qq.com>
2024-07-15 22:02:18 +0800

    9-13制作车次下拉框组件

--- admin/src/views/main/train-station.vue
+++ admin/src/views/main/train-station.vue
@@ -28,12 +28,7 @@
            ok-text="确认" cancel-text="取消">
     <a-form :model="trainStation" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
       <a-form-item label="车次编号">
-        <a-select v-model:value="trainStation.trainCode" show-search
-                  :filter-option="filterTrainCodeOption">
-          <a-select-option v-for="item in trains" :key="item.code" :value="item.code" :label="item.code+item.start+item.end">
-            {{item.code}}|{{item.start}}~{{item.end}}
-          </a-select-option>
-        </a-select>
+        <train-select-view v-model="trainStation.trainCode"></train-select-view>
       </a-form-item>
       <a-form-item label="站序">
         <a-input v-model:value="trainStation.index" />
