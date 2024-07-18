9b0d58740cca35d3945781b1829e2007965c68d0
zhaopei <424481223@qq.com>
2024-07-19 00:47:55 +0800

    10-4 演示SpringBoot自带的定时任务

---
+++ batch/src/main/java/com/zhaopei/train/batch/job/SpringBootTestJob.java
@@ -0,0 +1,21 @@
+package com.zhaopei.train.batch.job;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+// /**
+//  * 1、适合单体应用，不适合集群
+//  * 2、没法实时更改定时任务状态和策略
+//  */
+
+@Component
+@EnableScheduling
+public class SpringBootTestJob {
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    private void test(){
+        //增加分布式锁，解决集群问题。即使如此也只能结局问题1，解决不了问题2。
+        System.out.println("SpringBootTestJob TEST");
+    }
+
+}
