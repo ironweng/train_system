e75b40c54ad679933581e4270197a9dfb7492ef6
zhaopei <424481223@qq.com>
2024-07-20 12:56:30 +0800

    10-6 设置禁止跑批任务并发执行

--- batch/src/main/java/com/zhaopei/train/batch/job/TestJob.java
+++ batch/src/main/java/com/zhaopei/train/batch/job/TestJob.java
@@ -1,13 +1,21 @@
 package com.zhaopei.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob TEST");
+        System.out.println("TestJob 开始");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("TestJob 结束");
     }
 }
