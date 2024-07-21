a5ebaf3be33d94dc6952f13078b64741d33d799e
zhaopei <424481223@qq.com>
2024-07-21 23:05:11 +0800

    10-8 对菜单进行分类，改为两级菜单，路由也改为多级路由

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -10,24 +10,29 @@ const routes = [{
       path: 'about',
       component: () => import('../views/main/about.vue'),
     },{
-      path: 'station',
-      component: () => import('../views/main/station.vue'),
+        path: 'base/',
+        children: [{
+            path: 'station',
+            component: () => import('../views/main/base/station.vue'),
+        }, {
+            path: 'train',
+            component: () => import('../views/main/base/train.vue'),
+        }, {
+            path: 'train-station',
+            component: () => import('../views/main/base/train-station.vue'),
+        }, {
+            path: 'train-carriage',
+            component: () => import('../views/main/base/train-carriage.vue'),
+        }, {
+            path: 'train-seat',
+            component: () => import('../views/main/base/train-seat.vue'),
+        }]
     },{
-        path: 'train',
-        component: () => import('../views/main/train.vue'),
-    },{
-        path: 'train-station',
-        component: () => import('../views/main/train-station.vue'),
-    },{
-        path: 'train-carriage',
-        component: () => import('../views/main/train-carriage.vue'),
-    },{
-        path: 'train-seat',
-        component: () => import('../views/main/train-seat.vue'),
-    },{
-        path: 'batch/job',
-        name: 'batch/job',
-        component: () => import('../views/main/job.vue')
+        path: 'batch/',
+        children: [{
+            path: 'job',
+            component: () => import('../views/main/batch/job.vue')
+        }]
     }]
   }, {
     path: '',
