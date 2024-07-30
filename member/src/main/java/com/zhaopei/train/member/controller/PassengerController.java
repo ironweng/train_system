db9b8186cdc8f2401127b867377a170f16518dcf
赵培 <8830047+zhao-pei1228@user.noreply.gitee.com>
2024-07-31 00:13:09 +0800

    12-8 订单页面，查询我的所有乘客(可以在新增乘客的时候，增加一个校验，超过多少就不能再新增了。)

--- member/src/main/java/com/zhaopei/train/member/controller/PassengerController.java
+++ member/src/main/java/com/zhaopei/train/member/controller/PassengerController.java
@@ -11,6 +11,8 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/passenger")
 public class PassengerController {
@@ -35,5 +37,11 @@ public class PassengerController {
     public CommonResp<Object> delete(@PathVariable Long id){
         passengerService.delete(id);
         return new CommonResp<>();
+    }
+
+    @GetMapping("/query-mine")
+    public CommonResp<List<PassengerQueryResp>> queryMine() {
+        List<PassengerQueryResp> list = passengerService.queryMine();
+        return new CommonResp<>(list);
     }
 }
